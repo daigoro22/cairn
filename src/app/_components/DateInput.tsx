de4624a2ffb4f3fab50ea36d1665147e3b8b0a88
daigoro <daigoro.abe@systemi.co.jp>
2023-11-20 01:58:27 +0900

    fix(front): ビルドエラーを解消

--- src/app/_components/DateInput.tsx
+++ src/app/_components/DateInput.tsx
@@ -1,7 +1,7 @@
 import type { InputHTMLAttributes } from 'react';
 import { input } from './styles/input';
 
-type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {};
+type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>;
 
 export default function DateInput({ ...args }: Props) {
   return <input type="date" className={input()} {...args} />;
