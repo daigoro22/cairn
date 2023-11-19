0daa000ab6753736638c1de36bcd903748a30978
daigoro <daigoro.abe@systemi.co.jp>
2023-11-20 03:12:23 +0900

    feat(front): CheckBox コンポーネントを追加

---
+++ src/app/_components/CheckBox.tsx
@@ -0,0 +1,16 @@
+import { input, radioAndCheckboxContainer } from './styles/input';
+
+export default function CheckBoxInput({
+  name,
+  label,
+}: {
+  name: string;
+  label: string;
+}) {
+  return (
+    <div className={radioAndCheckboxContainer()}>
+      <input id={name} name={name} type="checkbox" className={input()} />
+      <label htmlFor={name}>{label}</label>{' '}
+    </div>
+  );
+}
