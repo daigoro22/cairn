a98c969e95863b9b58f8b9e583f84cb07fc4ebac
daigoro <daigoro.abe@systemi.co.jp>
2023-11-19 23:15:42 +0900

    build: panda css のデザイントークンを定義

--- panda.config.ts
+++ panda.config.ts
@@ -11,7 +11,32 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    tokens: {
+      sizes: {
+        sm: { value: '0.625rem' },
+        md: { value: '1rem' },
+        bg: { value: '1.5rem' },
+        xl: { value: '2rem' },
+        border: { input: { value: '1px' } },
+      },
+      colors: {
+        primary: { light: { value: '#7FFFD4' }, dark: { value: '#40806A' } },
+        secondary: { light: { value: '#FFD47F' }, dark: { value: '#8F7747' } },
+        tertiary: { light: { value: '#D47FFF' }, dark: { value: '#6A4080' } },
+      },
+      borders: {
+        primary: {
+          value: '{sizes.border.input} solid {colors.primary.dark}',
+        },
+      },
+      fontSizes: {
+        sm: { value: '{sizes.sm}' },
+        md: { value: '{sizes.md}' },
+      },
+      radii: {
+        input: { value: '{sizes.sm}' },
+      },
+    },
   },
 
   // The output directory for your css system
