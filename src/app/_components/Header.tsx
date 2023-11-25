1069051b6daf5c8c39b46bb63adb47eb9bcb223e
daigoro <daigoro.abe@systemi.co.jp>
2023-11-25 21:25:20 +0900

    feat(front): ヘッダをコンポーネント化

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,85 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { css } from 'styled-system/css';
+import ImageContainer from './ImageContainer';
+import TextInput from './TextInput';
+import Button from './Button';
+
+export default function Header() {
+  return (
+    <header
+      className={css({
+        height: 'header',
+        bg: 'white',
+        display: 'grid',
+        gridTemplateColumns: 'subgrid',
+        gridColumn: '1/13',
+        alignItems: 'center',
+      })}
+    >
+      <Link
+        href="/"
+        className={css({
+          gridColumn: '1/3',
+          alignItems: 'center',
+        })}
+      >
+        <div
+          className={css({
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+          })}
+        >
+          <ImageContainer size="header">
+            <Image
+              src="/icon.png"
+              sizes="100vw"
+              fill
+              objectFit="cover"
+              alt="cairn"
+            />
+          </ImageContainer>
+          <h1 className={css({ fontSize: 'xl' })}>Cairn</h1>
+        </div>
+      </Link>
+      <div
+        className={css({
+          gridColumn: '9/11',
+        })}
+      >
+        <TextInput
+          variants={{ marginX: 'none' }}
+          id="search"
+          type="search"
+          placeholder="検索"
+        />
+      </div>
+      <div
+        className={css({
+          gridColumn: '11/12',
+          margin: 'auto',
+        })}
+      >
+        <ImageContainer size="icon">
+          <Image
+            src="/icon.png"
+            sizes="100vw"
+            fill
+            objectFit="cover"
+            alt="cairn"
+          />
+        </ImageContainer>
+      </div>
+      <div
+        className={css({
+          gridColumn: '12/13',
+          margin: 'auto',
+        })}
+      >
+        <Button>投稿</Button>
+      </div>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import { css } from 'styled-system/css';
 import './globals.css';
-import Image from 'next/image';
-import ImageContainer from './_components/ImageContainer';
-import Link from 'next/link';
-import Button from './_components/Button';
-import TextInput from './_components/TextInput';
+import Header from './_components/Header';
 
 export const metadata = {
   title: 'Next.js',
@@ -26,80 +22,7 @@ export default function RootLayout({
           gridColumnGap: 'gridGap.main',
         })}
       >
-        <header
-          className={css({
-            height: 'header',
-            bg: 'white',
-            display: 'grid',
-            gridTemplateColumns: 'subgrid',
-            gridColumn: '1/13',
-            alignItems: 'center',
-          })}
-        >
-          <Link
-            href="/"
-            className={css({
-              gridColumn: '1/3',
-              alignItems: 'center',
-            })}
-          >
-            <div
-              className={css({
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-              })}
-            >
-              <ImageContainer size="header">
-                <Image
-                  src="/icon.png"
-                  sizes="100vw"
-                  fill
-                  objectFit="cover"
-                  alt="cairn"
-                />
-              </ImageContainer>
-              <h1 className={css({ fontSize: 'xl' })}>Cairn</h1>
-            </div>
-          </Link>
-          <div
-            className={css({
-              gridColumn: '9/11',
-            })}
-          >
-            <TextInput
-              variants={{ marginX: 'none' }}
-              id="search"
-              type="search"
-              placeholder="検索"
-            />
-          </div>
-          <div
-            className={css({
-              gridColumn: '11/12',
-              margin: 'auto',
-            })}
-          >
-            <ImageContainer size="icon">
-              <Image
-                src="/icon.png"
-                sizes="100vw"
-                fill
-                objectFit="cover"
-                alt="cairn"
-              />
-            </ImageContainer>
-          </div>
-          <div
-            className={css({
-              gridColumn: '12/13',
-              margin: 'auto',
-            })}
-          >
-            <Button>投稿</Button>
-          </div>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
