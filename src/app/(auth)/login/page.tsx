37e7dcfd41a1772395f503ec509c7d8a0cd4d7f0
daigoro <daigoro.abe@systemi.co.jp>
2023-11-26 16:35:42 +0900

    feat(front): ログインページを作成

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,29 +1,57 @@
 'use client';
 
+import Button from '@/app/_components/Button';
+import InputContainer from '@/app/_components/InputContainer';
+import TextInput from '@/app/_components/TextInput';
+import { mainAreaLabel } from '@/app/_components/styles/display';
+import { card, mainAreaGrid } from '@/app/_components/styles/layout';
 import { supabase } from '@/utils/supabase';
 import { useState } from 'react';
+import { css } from 'styled-system/css';
 
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <main>
-      <input type="text" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <button
-        onClick={() => {
-          supabase.auth
-            .signInWithPassword({
-              email,
-              password,
-            })
-            .then(console.log)
-            .catch(console.error);
-        }}
+    <main className={mainAreaGrid()}>
+      <section
+        className={css({
+          gridColumn: '5/9',
+        })}
       >
-        ログイン
-      </button>
+        <h1 className={mainAreaLabel()}>ログイン</h1>
+        <form>
+          <div className={card()}>
+            <InputContainer label="メールアドレス">
+              <TextInput
+                id="email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                variants={{ marginX: 'none', width: 'fill' }}
+              />
+            </InputContainer>
+            <InputContainer label="パスワード">
+              <TextInput
+                id="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                variants={{ marginX: 'none', width: 'fill' }}
+              />
+            </InputContainer>
+            <Button
+              onClick={() => {
+                supabase.auth
+                  .signInWithPassword({ email, password })
+                  .then(console.log)
+                  .catch(console.error);
+              }}
+            >
+              ログイン
+            </Button>
+          </div>
+        </form>
+      </section>
     </main>
   );
 }
