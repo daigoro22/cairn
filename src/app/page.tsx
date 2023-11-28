dc0d8bd85e51d0d14e2412927c0f6840bd63de92
daigoro <daigoro.abe@systemi.co.jp>
2023-11-28 23:55:33 +0900

    feat(front): トップページを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import { join } from '@/utils/panda';
+import { mainAreaGrid, subGrid } from './_components/styles/_layout';
+import { css } from 'styled-system/css';
+import ReviewCard from './_components/ReviewCard';
+
 export default function Home() {
-  return <main>aaa</main>;
+  return (
+    <main className={mainAreaGrid({ grid: 'lg' })}>
+      <section
+        className={join([
+          subGrid(),
+          css({ gridColumn: '3/11', gridRow: '1/13' }),
+        ])}
+      >
+        <div
+          className={css({
+            gridColumn: '1/9',
+            gridRow: '1/13',
+            bg: 'white',
+            borderRadius: 'card.md',
+            display: 'flex',
+            gap: '2rem',
+            padding: 'xl',
+            justifyContent: 'space-evenly',
+            flexWrap: 'wrap',
+          })}
+        >
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+        </div>
+      </section>
+    </main>
+  );
 }
