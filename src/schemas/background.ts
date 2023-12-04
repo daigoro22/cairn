7525403793c5e0a4cc574aaa4de8612028e55af8
daigoro <daigoro.abe@systemi.co.jp>
2023-12-05 01:50:30 +0900

    feat: 経歴登録APIのスキーマを object の array にした

--- src/schemas/background.ts
+++ src/schemas/background.ts
@@ -2,14 +2,18 @@ import { z } from 'zod';
 import { dateZodObject } from './common';
 
 export const backgroundEditApiSchema = z.object({
-  id: z.string().uuid().nullish(),
-  name: z
-    .string()
-    .min(1, '名前を入力してください')
-    .max(100, '名前は100文字以下になるようにしてください'),
-  schoolCode: z.string().nullish(),
-  startDate: dateZodObject,
-  endDate: dateZodObject.nullish(),
+  items: z.array(
+    z.object({
+      backgroundId: z.string().uuid().nullish(),
+      organizationName: z
+        .string()
+        .min(1, '名前を入力してください')
+        .max(100, '名前は100文字以下になるようにしてください'),
+      schoolCode: z.string().nullish(),
+      startDate: dateZodObject,
+      endDate: dateZodObject.nullish(),
+    }),
+  ),
 });
 
 export type BackgroundEditApiSchema = z.infer<typeof backgroundEditApiSchema>;
