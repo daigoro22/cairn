a0e7dedc83a94519df5ff6fa1465c32a6d03ca24
daigoro <daigoro.abe@systemi.co.jp>
2023-12-04 12:47:49 +0900

    refactor(back): supabase クライアントを共通処理で取得できるようにした

---
+++ src/utils/supabase.ts
@@ -0,0 +1,26 @@
+import type { CookieOptions } from '@supabase/ssr';
+import { createServerClient } from '@supabase/ssr';
+import type { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+
+export const createRouterClient = (cookieStore: ReadonlyRequestCookies) =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: routerCookie(cookieStore),
+    },
+  );
+
+const routerCookie = (cookieStore: ReadonlyRequestCookies) => ({
+  get(name: string) {
+    return cookieStore.get(name)?.value;
+  },
+  set(name: string, value: string, options: CookieOptions) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    cookieStore.set(name, value, options);
+  },
+  remove(name: string, options: CookieOptions) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    cookieStore.set(name, '', options);
+  },
+});
