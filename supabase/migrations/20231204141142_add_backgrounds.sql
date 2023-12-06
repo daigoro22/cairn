ae4bd96265d9d7c896c5ffe43583f1b07e2c154b
daigoro <daigoro.abe@systemi.co.jp>
2023-12-06 20:47:34 +0900

    feat: 経歴テーブルのマイグレーション

--- supabase/migrations/20231204141142_add_backgrounds.sql
+++ supabase/migrations/20231204141142_add_backgrounds.sql
@@ -5,24 +5,10 @@ CREATE TABLE IF NOT EXISTS backgrounds (
     name VARCHAR(255) not null,
     school_code VARCHAR(13),
     start_date DATE not null,
-    end_date DATE not null
+    end_date DATE
 );
 
 ALTER TABLE backgrounds ENABLE ROW LEVEL SECURITY;
 
 create policy "Users can update their own backgrounds."
   on backgrounds for all using (auth.uid() = user_id) with check (auth.uid() = user_id);
-
--- DROP FUNCTION IF EXISTS public.update_backgrounds();
--- CREATE OR REPLACE FUNCTION public.update_backgrounds(new_name TEXT, new_school_code TEXT, new_start_date DATE, new_end_date DATE)
--- returns setof backgrounds
--- language plpgsql
--- security invoker
--- set search_path = public
--- as $$
--- begin
---   delete from backgrounds;
---   insert backgrounds (user_id, name, school_code, start_date, auth.uid(), name=new_name, school_code=new_school_code, start_date=new_start_date, end_date=new_end_date;
---   return new;
--- end;
--- $$;
