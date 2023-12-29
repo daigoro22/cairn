2b3f2fd86e19a6090ce6804508ec13ee5a15301d
daigoro <daigoro.abe@systemi.co.jp>
2023-12-30 01:20:20 +0900

    fix(front): 画像表示するドメインを修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['127.0.0.1', 'thumbnail.image.rakuten.co.jp'] },
+  images: {
+    domains: [
+      '127.0.0.1',
+      'thumbnail.image.rakuten.co.jp',
+      `${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`,
+    ],
+  },
 };
 
 module.exports = nextConfig;
